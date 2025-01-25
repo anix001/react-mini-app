f2461a8a63495b4e1b245031c09694dd1781e3e5
anix001 <chaudharyanish305@gmail.com>
2025-01-25 23:14:59 +0545

    feat: folder str for useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 // import DebounceSearchBarComponent from './concepts/throttle-and-debounce/Debounce'
 // import ThrottledScrollComponent from './concepts/throttle-and-debounce/Throttle'
-import UseReducerForm from './concepts/throttle-and-debounce/UseReducer'
+import UseReducerForm from './concepts/useReducer/UseReducer'
 
 function App() {
 
